8689b3a566f17d09c5171e322c277a474f097364
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2025-08-03 14:45:14 +0530

    changes made for document compare folder and rest files

--- document_portal/utils/model_loader.py
+++ document_portal/utils/model_loader.py
@@ -1,5 +1,4 @@
 
-
 import os
 import sys
 from dotenv import load_dotenv
@@ -84,7 +83,7 @@ class ModelLoader:
         elif provider == "groq":
             llm=ChatGroq(
                 model=model_name,
-                api_key=self.api_keys["GROQ_API_KEY"],
+                api_key=self.api_keys["GROQ_API_KEY"], #type: ignore
                 temperature=temperature,
             )
             return llm
@@ -119,6 +118,4 @@ if __name__ == "__main__":
     
     # Test the ModelLoader
     result=llm.invoke("Hello, how are you?")
-    print(result)
     print(f"LLM Result: {result.content}")
-    print("ALL COMPLETE!")
